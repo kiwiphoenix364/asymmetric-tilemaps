4003e0d51ddd797cba0597d9597fafcb1ebd50e7
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-08-27 22:04:19 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -15,8 +15,25 @@ let list = [img`
     ......33555533......
     ........3553........
     .........33.........
+    `, img`
+    .........33.........
+    ........3553........
+    ......33555533......
+    .....3555555553.....
+    ....355555555553....
+    ..3355555555555533..
+    .355555555555555553.
+    35555555555555555553
+    35555555555555555553
+    .355555555555555553.
+    ..3355555555555533..
+    ....355555555553....
+    .....3555555553.....
+    ......33555533......
+    ........3553........
+    .........33.........
     `]
-let tile = img`
+let tileMapImg = img`
     1 . . . . . . . . . . . . . . .
     1 . . . . . . . . . . . . . . .
     . 1 . . . . . . . . . . . . . .
@@ -36,22 +53,22 @@ let tile = img`
 `
 let zLayer = 0
 let buf = Buffer.create(120)
-let img1 = image.create(0,0)
-for (let x1 = 0; x1 < 9; x1++) {
-    for (let y1 = 0; y1 < 8; y1++) {
-        redrawImg.drawImg(x1 * 20, y1 * 16, list[tile.getPixel(x1, y1)], img1)
-    }
-}
 let variable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
+    for (let x1 = 0; x1 < 9; x1++) {
+        for (let y1 = 0; y1 < 8; y1++) {
+            redrawImg.drawImg(x1 * 20 + y1 % 2 * 10, y1 * 16 - y1 % 2 * 8, list[tileMapImg.getPixel(x1, y1)], image)
+        }
+    }
     for (let index = 0; index < 160; index++) {
-    image.setRows(index, buf)
     }
 })
 namespace redrawImg {
     export function drawImg(x: number, y: number, startImg: Image, targetImg: Image) {
         for (let xIdx = 0; xIdx < 20; xIdx++) {
             for (let yIdx = 0; yIdx < 16; yIdx++) {
-                targetImg.setPixel(x + xIdx, y + yIdx, startImg.getPixel(xIdx, yIdx))
+                if (startImg.getPixel(xIdx, yIdx) != 0) {
+                    targetImg.setPixel(x + xIdx, y + yIdx, startImg.getPixel(xIdx, yIdx))
+                }
             }
         }
     }
