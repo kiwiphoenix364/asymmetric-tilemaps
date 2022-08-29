a56b3408f5beb13dae7c5d1972c694110cb9270a
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-08-29 08:32:03 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -151,7 +151,7 @@ let variable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camer
     }
     for (let x1 = 0; x1 < 10; x1++) {
         for (let y1 = 0; y1 < 18; y1++) {
-            redrawImg.drawImg(x1 * 20 + (y1 + Math.round(camera.top / 16)) % 2 * 10 - 10 - camera.x % 20, y1 * 8 - 8 - camera.top % 16, list[tileMapImg.getPixel(x1 + camera.left, y1 + Math.round(camera.top / 2) * 2)], image)
+            redrawImg.drawImg(x1 * 20 + (y1 + Math.round(camera.top / 16)) % 2 * 10 - 10 - camera.x % 20, y1 * 8 - 8 - camera.top % 16, list[tileMapImg.getPixel(x1 + camera.left / 20, y1 + Math.round(camera.top / 2) * 2)], image)
         }
     }
     for (let index = 0; index < 160; index++) {
